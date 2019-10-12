825e4c3ba4809ce3aad92b94feb98403e66348dc
Admin <Admin@DESKTOP-4VSMM3J>
2019-10-12 20:39:22 +0530

    this is edited method2

--- QCCM10/src/com/anthem/crm/pomrepository/Quote.java
+++ QCCM10/src/com/anthem/crm/pomrepository/Quote.java
@@ -2,8 +2,9 @@ package com.anthem.crm.pomrepository;
 
 public class Quote {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void method1() 
+	{
+		
 
 	}
 
